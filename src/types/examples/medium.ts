94685463e408719a108ceab909bafe4483aa3b0d
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-09-30 10:47:29 +0300

    Add medium example

---
+++ src/types/examples/medium.ts
@@ -0,0 +1,85 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { v4 as uuidv4 } from "uuid";
+import {
+  Char,
+  Color,
+  FontFamily,
+  History,
+  Image,
+  Operation,
+  Position,
+  Presentation,
+  Size,
+  Slide,
+  Text,
+} from "../types";
+
+const fontFamily: FontFamily = "Arial";
+
+const color: Color = "#aaaaaa";
+
+const size: Size = {
+  width: 400,
+  height: 500,
+};
+
+const position: Position = {
+  x: 12,
+  y: 90,
+};
+
+const char: Char = {
+  value: "A",
+  fontSize: 12,
+  fontFamily: fontFamily,
+  color: color,
+  bold: false,
+  italic: true,
+  underlined: false,
+};
+
+const text: Text = {
+  id: uuidv4(),
+  position: position,
+  size: size,
+  rotation: 0,
+  type: "TEXT",
+  content: [char, char],
+};
+
+const image: Image = {
+  id: uuidv4(),
+  position: position,
+  size: size,
+  rotation: 0,
+  type: "IMAGE",
+  content: "base64:image/jpeg...",
+};
+
+const slide: Slide = {
+  id: uuidv4(),
+  background: color,
+  selectObjects: [text],
+  objects: [image, text],
+};
+
+// @ts-ignore
+const presentation: Presentation = {
+  name: "The first",
+  currentSlide: slide,
+  selectSlides: [],
+  slides: [slide],
+};
+
+const operation: Operation = {
+  id: uuidv4(),
+  data: {},
+  prev: null,
+  next: null,
+};
+
+// @ts-ignore
+const history: History = {
+  topOperation: operation,
+};
