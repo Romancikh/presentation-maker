42e9a5a223005dd500875afc80f0b194b1172761
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-10-13 21:48:51 +0300

    Update Workspace component

--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -2,11 +2,12 @@ import { Slide } from "../../types/types.ts";
 import classes from "./Workspace.module.css";
 
 type WorkspaceProps = {
-  slide: Slide;
+  slide: Slide | null;
 };
 
+// TODO: 1. Добавить логику пустого слайда
 function Workspace({ slide }: WorkspaceProps) {
-  return <div className={classes.workspace}>{slide.id}</div>;
+  return <div className={classes.workspace}>{slide ? slide.id : ""}</div>;
 }
 
 export default Workspace;
