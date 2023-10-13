b550b739e3252278da04f058a3c0726f680b5782
Kutimskii Roman <kutimskiy.roma@gmail.com>
2023-10-13 22:21:13 +0300

    Update Button component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,28 @@
-import classes from "./Button.module.css";
+import "./Button.css";
+import classNames from "classnames";
 
 type ButtonProps = {
   text?: string;
   icon?: string;
+  position: "menubar" | "toolbar";
 };
 
-function Button({ text, icon }: ButtonProps) {
+function Button({ text, icon, position }: ButtonProps) {
+  const isIconButton = icon && !text;
+  const isTextButton = text && !icon;
+
   return (
-    <button className={icon ? classes.button__icon : classes.button__text}>
-      {icon && <span className="material-symbols-outlined">{icon}</span>}
-      {text}
+    <button
+      className={classNames("button", {
+        button_icon: isIconButton,
+        button_text: isTextButton,
+        [`button_${position}`]: position,
+      })}
+    >
+      {isIconButton && (
+        <span className="button__icon material-symbols-outlined">{icon}</span>
+      )}
+      {isTextButton && <span className="button__text">{text}</span>}
     </button>
   );
 }
