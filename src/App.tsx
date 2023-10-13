0dd397219f3789bc3c3d5554b5c7bc84ffa9fedc
555Jeka555 <2003antyshev@mail.ru>
2023-10-13 14:18:56 +0300

    Add init new presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Presentation } from "./types/types.ts";
+import { Color, Presentation, Slide } from "./types/types.ts";
 import Header from "./components/Header/Header.tsx";
 import SlideBar from "./components/SlideBar/SlideBar.tsx";
 import Workspace from "./components/Workspace/Workspace.tsx";
@@ -9,6 +9,23 @@ type AppProps = {
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 function App({ presentation }: AppProps) {
+  function initNewPresentation() {
+    const background: Color = "#fff";
+
+    const newSlide: Slide = {
+      background: background,
+      id: "1",
+      objects: [],
+      selectObjects: [],
+    };
+
+    presentation.name = "";
+    presentation.slides = [newSlide];
+    presentation.currentSlide = newSlide;
+  }
+
+  initNewPresentation();
+
   return (
     <div>
       <Header />
