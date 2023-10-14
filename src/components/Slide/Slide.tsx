e3d43d3e5a5be7c7a361a6e77d272b1c4226cfc0
555Jeka555 <2003antyshev@mail.ru>
2023-10-14 12:17:16 +0300

    Add maps object in Workspace and create simple preview

--- src/components/Slide/Slide.tsx
+++ src/components/Slide/Slide.tsx
@@ -1,6 +1,9 @@
 import "./Slide.css";
 import { CSSProperties } from "react";
 import { Slide } from "../../types/types.ts";
+import Text from "../Text/Text.tsx";
+import Image from "../Image/Image.tsx";
+import Primitive from "../Primitive/Primitive.tsx";
 
 type SlidePreviewProps = {
   index: number;
@@ -15,7 +18,39 @@ function Slide({ index, slide }: SlidePreviewProps) {
   return (
     <div className={"slide"}>
       <div>{index}</div>
-      <div className={"slide__preview"} style={style}></div>
+      <div className={"slide__preview"} style={style}>
+        {slide.objects.map((object) => {
+          if (object.type === "text") {
+            return (
+              <Text
+                text={object.content}
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+              />
+            );
+          } else if (object.type === "image") {
+            return (
+              <Image
+                src={object.content}
+                alt={object.alt}
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+              />
+            );
+          } else if (object.type === "primitive") {
+            return (
+              <Primitive
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+                form={object.form}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
--- src/components/Workspace/Workspace.tsx
+++ src/components/Workspace/Workspace.tsx
@@ -1,5 +1,8 @@
 import "./Workspace.css";
 import { Slide } from "../../types/types.ts";
+import Text from "../Text/Text.tsx";
+import Image from "../Image/Image.tsx";
+import Primitive from "../Primitive/Primitive.tsx";
 
 type WorkspaceProps = {
   slide: Slide | null;
@@ -7,7 +10,45 @@ type WorkspaceProps = {
 
 // TODO: 1. Добавить логику пустого слайда
 function Workspace({ slide }: WorkspaceProps) {
-  return <div className="workspace">{slide ? slide.id : ""}</div>;
+  return (
+    <div className="workspace">
+      {slide ? (
+        slide.objects.map((object) => {
+          if (object.type === "text") {
+            return (
+              <Text
+                text={object.content}
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+              />
+            );
+          } else if (object.type === "image") {
+            return (
+              <Image
+                src={object.content}
+                alt={object.alt}
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+              />
+            );
+          } else if (object.type === "primitive") {
+            return (
+              <Primitive
+                position={object.position}
+                size={object.size}
+                rotation={object.rotation}
+                form={object.form}
+              />
+            );
+          }
+        })
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
 
 export default Workspace;
