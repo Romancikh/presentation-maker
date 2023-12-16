a5d386b4665cbcb7a6b7eee9b0ecf25de1f5ff87
Roman Kutimskii <kutimskiy.roma@gmail.com>
2023-12-16 17:56:08 +0300

    Fix ts errors

--- src/constants/MenuBar.ts
+++ src/constants/MenuBar.ts
@@ -2,31 +2,16 @@ import { v4 as uuidv4 } from "uuid";
 import { Menu, MenuElement } from "../types/types.ts";
 
 const fileMenuElements: MenuElement[] = [
-  {
-    id: uuidv4(),
-    text: "Создать",
-  },
-
-  {
-    id: uuidv4(),
-    text: "Скачать",
-  },
+  { id: uuidv4(), text: "Создать", onClick: () => {} },
+  { id: uuidv4(), text: "Скачать", onClick: () => {} },
 ];
 export const fileMenu: Menu = {
   menuElements: fileMenuElements,
 };
 
 const correctionMenuElements: MenuElement[] = [
-  {
-    id: uuidv4(),
-    shortcut: "Ctrl+Z",
-    text: "Отменить",
-  },
-  {
-    id: uuidv4(),
-    shortcut: "Ctrl+Y",
-    text: "Повторить",
-  },
+  { id: uuidv4(), shortcut: "Ctrl+Z", text: "Отменить", onClick: () => {} },
+  { id: uuidv4(), shortcut: "Ctrl+Y", text: "Повторить", onClick: () => {} },
 ];
 export const correctionMenu: Menu = {
   menuElements: correctionMenuElements,
